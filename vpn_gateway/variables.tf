b7f258ad7db656079514379dfaa04a621f3cd955
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-11-22 19:21:30 +0100

    Update variables.tf

--- vpn_gateway/variables.tf
+++ vpn_gateway/variables.tf
@@ -6,6 +6,8 @@ variable "resource_group_name" {
   description = "Name of resource group to deploy resources in."
 }
 
+
+
 variable "location" {
   description = "The Azure Region in which to create resource."
 }
